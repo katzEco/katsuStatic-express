f1e5dda8acdf9e915d92f0633af62c44e5a06e94
dethMastery <me@suphakit.net>
2023-01-18 17:14:23 +0700

    updated: setting up page

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ let serveIndex = require('serve-index')
 function middleRun(exp, app, root) {
   app.use(exp.static('static'))
 
-  app.use(app.use('/', serveIndex('static/files', {'template': root + 'static/fileServe.html'})))
+  app.use('/', serveIndex('static/files', {'template': root + '/static/staticPage/fileServe.html'}))
 }
 
 module.exports = {
--- src/page.js
+++ src/page.js
@@ -1,5 +1,7 @@
 function pageRun(app, root) {
-
+  app.get('/mainCSS', (req, res) => {
+    res.sendFile(root + '/static/staticPage/main.css')
+  })
 }
 
 module.exports = {
