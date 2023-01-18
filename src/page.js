521c68166a3d5ecbaac483713261573829cbc88f
Suphakit P <me@suphakit.net>
2023-01-19 03:34:00 +0700

    updated: mobile responsive

--- src/page.js
+++ src/page.js
@@ -2,6 +2,9 @@ function pageRun(app, root) {
   app.get('/mainCSS', (req, res) => {
     res.sendFile(root + '/static/staticPage/css/main.css')
   })
+  app.get('/mobileCSS', (req, res) => {
+    res.sendFile(root + '/static/staticPage/css/mobile.css')
+  })
 
   app.get('/about', (req, res) => {
     res.sendFile(root + '/static/staticPage/about.html')
