e147b266c74acc8b64239d8e8fd07976c3778a33
dethMastery <me@suphakit.net>
2023-01-18 19:31:43 +0700

    feat: setting up about page

--- src/page.js
+++ src/page.js
@@ -2,6 +2,10 @@ function pageRun(app, root) {
   app.get('/mainCSS', (req, res) => {
     res.sendFile(root + '/static/staticPage/css/main.css')
   })
+
+  app.get('/about', (req, res) => {
+    res.sendFile(root + '/static/staticPage/about.html')
+  })
 }
 
 module.exports = {
